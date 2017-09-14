f7d2e14642118899a3b7df1a2592937eed45b7d3
Simon Glantz <simon.glantz@mac.com>
2017-09-14 22:42:55 +0100

    sg - added deploy scripts

---
+++ deploy/test.ps1
@@ -0,0 +1,27 @@
+$testFile = ".\WebApp\WebApplication.Tests\bin\Release\K9.WebApplication.Tests.dll"
+	
+function ProcessErrors(){
+  if($? -eq $false)
+  {
+    throw "The previous command failed (see above)";
+  }
+}
+
+function _Test() {
+  echo "Running tests"
+  
+  packages\xunit.runner.console.2.2.0\tools\xunit.console.exe $testFile
+  ProcessErrors
+}
+
+function Main {
+  Try {
+	_Test
+  }
+  Catch {
+    Write-Error $_.Exception
+    exit 1
+  }
+}
+
+Main
